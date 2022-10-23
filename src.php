1f3499e47ac1148725379af72e2209c05cfaed89
Archt111 <94046542+Archt111@users.noreply.github.com>
2022-10-23 19:14:48 +0300

    Add files via upload
    Store your php code in a file name src.

---
+++ src.php
@@ -0,0 +1,9 @@
+ <!DOCTYPE html>  
+     <head>  
+      <title>Hello World!</title>
+     </head>  
+
+     <body>  
+      <h1>WTF, it's so late now. Hurry up!</h1>
+      <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+     </body>
