b1d785eb6e2143ec4a17ebef9376cfffa7d35470
Archt111 <94046542+Archt111@users.noreply.github.com>
2022-10-26 08:42:45 +0300

    Update src.php

--- src.php
+++ src.php
@@ -4,6 +4,6 @@
      </head>  
 
      <body>  
-      <h1>WTF, it's so late now. Hurry up!</h1>
+      <h1>Late now. Hurry up!</h1>
       <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
      </body>
